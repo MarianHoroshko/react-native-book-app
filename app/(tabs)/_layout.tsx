56dc6c764c00d69e2159af1575848ba1f0845cd9
marian <marian.horoshko@gmail.com>
2025-05-24 13:14:03 +0200

    add tabs and font awesome

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+import { Tabs } from "expo-router";
+
+const _Layout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{ title: "Home", headerShown: false }}
+      />
+
+      <Tabs.Screen
+        name="search"
+        options={{ title: "Search", headerShown: false }}
+      />
+    </Tabs>
+  );
+};
+
+export default _Layout;
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { faMugSaucer } from "@fortawesome/free-solid-svg-icons/faMugSaucer";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl">Edit app/index.tsx to edit this screen.</Text>
+      <FontAwesomeIcon icon={faMugSaucer} />
     </View>
   );
 }
