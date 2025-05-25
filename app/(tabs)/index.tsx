9e97fd4b1953ae9863d6e7727e4a6b19660d62c3
marian <marian.horoshko@gmail.com>
2025-05-25 14:37:37 +0200

    basic home layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,38 +1,92 @@
+import BooksSection from "@/components/BooksSetion";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import {
   FlatList,
-  SafeAreaView,
+  ScrollView,
   Text,
   TouchableOpacity,
   View,
 } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const listData = ["test", "test", "test"];
 
   return (
-    <SafeAreaView className="flex-1 border-none bg-white">
-      {/* header */}
-      <View className="flex-row justify-between w-100 h-5 m-5 items-center">
-        <Text className="text-xl font-bold color-green-500">book-app</Text>
+    <SafeAreaProvider>
+      <SafeAreaView>
+        <ScrollView>
+          <View className="flex-col bg-white">
+            {/* header */}
+            <View className="flex-row justify-between w-100 h-10 m-5 items-center">
+              <Text className="text-xl font-bold color-green-500">
+                book-app
+              </Text>
 
-        <TouchableOpacity className="bg-green-500 p-2 rounded-full">
-          <FontAwesomeIcon icon={faSearch} color="#fff" />
-        </TouchableOpacity>
-      </View>
+              <TouchableOpacity className="bg-green-500 p-2 rounded-full">
+                <FontAwesomeIcon icon={faSearch} color="#fff" />
+              </TouchableOpacity>
+            </View>
 
-      {/* scrollable list */}
-      <View className="w-100 m-5">
-        <Text className="text-lg">Section</Text>
+            {/* scrollable list */}
+            <View className="w-100 h-100 m-5">
+              <Text className="text-lg">Section</Text>
 
-        <FlatList
-          horizontal
-          data={listData}
-          renderItem={({ item }) => <Text className="p-5">{item}</Text>}
-          // keyExtractor={}
-        />
-      </View>
-    </SafeAreaView>
+              <FlatList
+                horizontal
+                data={listData}
+                renderItem={({ item }) => <BooksSection item={item} />}
+                // keyExtractor={}
+              />
+            </View>
+
+            <View className="w-100 h-100 m-5">
+              <Text className="text-lg">Section</Text>
+
+              <FlatList
+                horizontal
+                data={listData}
+                renderItem={({ item }) => <BooksSection item={item} />}
+                // keyExtractor={}
+              />
+            </View>
+
+            <View className="w-100 h-100 m-5">
+              <Text className="text-lg">Section</Text>
+
+              <FlatList
+                horizontal
+                data={listData}
+                renderItem={({ item }) => <BooksSection item={item} />}
+                // keyExtractor={}
+              />
+            </View>
+
+            <View className="w-100 h-100 m-5">
+              <Text className="text-lg">Section</Text>
+
+              <FlatList
+                horizontal
+                data={listData}
+                renderItem={({ item }) => <BooksSection item={item} />}
+                // keyExtractor={}
+              />
+            </View>
+
+            <View className="w-100 h-100 m-5">
+              <Text className="text-lg">Section</Text>
+
+              <FlatList
+                horizontal
+                data={listData}
+                renderItem={({ item }) => <BooksSection item={item} />}
+                // keyExtractor={}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
---
+++ components/BooksSetion.tsx
@@ -0,0 +1,22 @@
+import { faBook } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import { Text, TouchableOpacity, View } from "react-native";
+
+const BooksSection = (props: { item: string }) => {
+  return (
+    <TouchableOpacity className="pr-3">
+      <View className="w-30 h-50 items-center align-middle bg-slate-100 rounded">
+        <FontAwesomeIcon
+          icon={faBook}
+          color="#475569"
+          size={32}
+          style={{ marginHorizontal: 50, marginVertical: 100 }}
+        />
+      </View>
+
+      <Text>{props.item}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default BooksSection;
