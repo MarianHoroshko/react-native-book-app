66dcdae26d068bbfe0f65c885d15953948b7b6bf
marian <marian.horoshko@gmail.com>
2025-05-25 14:06:12 +0200

    app header

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
+import {
+  FlatList,
+  SafeAreaView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function Index() {
+  const listData = ["test", "test", "test"];
+
   return (
-    <View>
-      <Text className="text-5xl">Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <SafeAreaView className="flex-1 border-none bg-white">
+      {/* header */}
+      <View className="flex-row justify-between w-100 h-5 m-5 items-center">
+        <Text className="text-xl font-bold color-green-500">book-app</Text>
+
+        <TouchableOpacity className="bg-green-500 p-2 rounded-full">
+          <FontAwesomeIcon icon={faSearch} color="#fff" />
+        </TouchableOpacity>
+      </View>
+
+      {/* scrollable list */}
+      <View className="w-100 m-5">
+        <Text className="text-lg">Section</Text>
+
+        <FlatList
+          horizontal
+          data={listData}
+          renderItem={({ item }) => <Text className="p-5">{item}</Text>}
+          // keyExtractor={}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
